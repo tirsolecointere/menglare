205c0d5c760838cc0c899389de398129089ce9d7
Tirso Lecointere <tirsolecointere@gmail.com>
2022-11-07 13:49:31 -0400

    install swiper

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,42 @@
         <div class="container">
             <div class="bg-white overflow-hidden sm:rounded-lg p-6">
                 <h1 class="text-3xl font-bold mb-4">Hello world</h1>
-                <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem earum, est blanditiis sapiente porro amet aliquid hic impedit. Suscipit temporibus optio dicta culpa. Minima recusandae eligendi explicabo nostrum excepturi quaerat, accusamus qui culpa fuga optio totam animi obcaecati numquam! Ipsa dolorem molestiae quae asperiores eius minima quibusdam non, dolore consequuntur.</p>
-                <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem earum, est blanditiis sapiente porro amet aliquid hic impedit. Suscipit temporibus optio dicta culpa. Minima recusandae eligendi explicabo nostrum excepturi quaerat, accusamus qui culpa fuga optio totam animi obcaecati numquam! Ipsa dolorem molestiae quae asperiores eius minima quibusdam non, dolore consequuntur.</p>
-                <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem earum, est blanditiis sapiente porro amet aliquid hic impedit. Suscipit temporibus optio dicta culpa. Minima recusandae eligendi explicabo nostrum excepturi quaerat, accusamus qui culpa fuga optio totam animi obcaecati numquam! Ipsa dolorem molestiae quae asperiores eius minima quibusdam non, dolore consequuntur.</p>
-                <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem earum, est blanditiis sapiente porro amet aliquid hic impedit. Suscipit temporibus optio dicta culpa. Minima recusandae eligendi explicabo nostrum excepturi quaerat, accusamus qui culpa fuga optio totam animi obcaecati numquam! Ipsa dolorem molestiae quae asperiores eius minima quibusdam non, dolore consequuntur.</p>
-                <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem earum, est blanditiis sapiente porro amet aliquid hic impedit. Suscipit temporibus optio dicta culpa. Minima recusandae eligendi explicabo nostrum excepturi quaerat, accusamus qui culpa fuga optio totam animi obcaecati numquam! Ipsa dolorem molestiae quae asperiores eius minima quibusdam non, dolore consequuntur.</p>
+
+                <div class="category__swiper | swiper my-8">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <div class="bg-green-100 rounded-lg p-4">
+                                Slider 1
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="bg-green-100 rounded-lg p-4">
+                                Slider 1
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="bg-green-100 rounded-lg p-4">
+                                Slider 1
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="bg-green-100 rounded-lg p-4">
+                                Slider 1
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="bg-green-100 rounded-lg p-4">
+                                Slider 1
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-pagination"></div>
+
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+                </div>
+
                 <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem earum, est blanditiis sapiente porro amet aliquid hic impedit. Suscipit temporibus optio dicta culpa. Minima recusandae eligendi explicabo nostrum excepturi quaerat, accusamus qui culpa fuga optio totam animi obcaecati numquam! Ipsa dolorem molestiae quae asperiores eius minima quibusdam non, dolore consequuntur.</p>
                 <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem earum, est blanditiis sapiente porro amet aliquid hic impedit. Suscipit temporibus optio dicta culpa. Minima recusandae eligendi explicabo nostrum excepturi quaerat, accusamus qui culpa fuga optio totam animi obcaecati numquam! Ipsa dolorem molestiae quae asperiores eius minima quibusdam non, dolore consequuntur.</p>
             </div>
