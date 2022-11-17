4eae3d7d9b3aafab21b3b208266c636c67f0a2a9
Tirso Lecointere <tirsolecointere@gmail.com>
2022-11-16 21:26:53 -0400

    create order relationships

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,6 +9,8 @@ class Order extends Model
 {
     use HasFactory;
 
+    protected $guarded = ['id', 'created_at', 'updated_at', 'status'];
+
     // order status
     const   PENDING = 1,
             RECEIVED = 2,
@@ -19,4 +21,20 @@ class Order extends Model
     // shipping types
     const   DELIVERY = 1,
             PICKUP = 2;
+
+    public function department() {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function city() {
+        return $this->belongsTo(City::class);
+    }
+
+    public function district() {
+        return $this->belongsTo(District::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
